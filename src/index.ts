e7fbca42c315500dd71378e52cd43bfef835e241
Ahamed <ahamedali95@gmail.com>
2021-11-20 18:55:38 -0500

    JOB-11: ts chages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-    console.log('Express listening on port ' + server.address().port);
+app.listen(app.get('port'), function() {
+    console.log('Express listening on port ' + process.env.PORT || 3000);
 });
-app.get('/time', function (req: any, res: any) {
-    res.send(new Date())
+app.get('/time', function (req, res) {
+    res.send(new Date());
 });
